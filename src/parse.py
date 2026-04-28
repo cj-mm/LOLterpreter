b2f77491c86db96dfa040fa27c48ec39004203db
cj-mm <cmmarasigan@up.edu.ph>
2026-04-28 23:48:41 +0800

    refactor: decouple the gui

--- src/parse.py
+++ src/parse.py
@@ -1,18 +1,15 @@
-from tkinter import *
+from errors import ParseError
+
 
 class Parser(object):
 
-    def __init__(self, tokens, tk):
-        self.console = tk.winfo_children()[23] # gui
-        # the tokens will be the one from the lexer.py
-        self.tokens = tokens
-        self.i = 0  #for iterating later
+    def __init__(self, tokens, on_error=None):
+        self.tokens = tokens  # the tokens will be the one from the lexer.py
+        self.i = 0
         self.errFlag = 0
+        self.on_error = on_error or (lambda msg: print(msg))
 
-
-    #########################################################
     # OTHERS/ESSENTIALS
-
     def linebreak(self):
         if self.tokens[self.i][1] == "EOL":
             self.i += 1
@@ -21,7 +18,16 @@ class Parser(object):
 
     # statements/code block (recursive)
     def statement(self):
-        if self.output() or self.input() or self.declaration() or self.assignment() or self.expr() or self.loops() or self.conditional() or self.typecast(): # check one of these if match
+        if (
+            self.output()
+            or self.input()
+            or self.declaration()
+            or self.assignment()
+            or self.expr()
+            or self.loops()
+            or self.conditional()
+            or self.typecast()
+        ):  # check one of these if match
             if self.next():
                 return True
         return False
@@ -33,35 +39,38 @@ class Parser(object):
                 return True
         return False
 
-
     def expr(self):
-        if self.arithmetic() or self.comparison() or self.boolean() or self.concat() or self.typecast():
+        if (
+            self.arithmetic()
+            or self.comparison()
+            or self.boolean()
+            or self.concat()
+            or self.typecast()
+        ):
             return True
         return False
 
-    
     def an_key(self):
         if self.tokens[self.i][1] == "AN":
             self.i += 1
             return True
         return False
 
-    
     def literal(self):
         literal = ["YARN", "TROOF", "NUMBR", "NUMBAR"]
         if self.tokens[self.i][0] in literal or self.tokens[self.i][1] in literal:
             self.i += 1
             return True
         return False
-    
 
     def identifier(self):
         if self.tokens[self.i][0] == "IDENTIFIER":
             self.i += 1
             return True
         return False
+
     #########################################################
-    
+
     #########################################################
     # CONDITIONAL
 
@@ -70,17 +79,28 @@ class Parser(object):
             return True
         return False
 
-
     def if_then_codeblock(self):
-        if self.output() or self.input() or self.declaration() or self.assignment() or self.expr() or self.loops() or self.conditional():
+        if (
+            self.output()
+            or self.input()
+            or self.declaration()
+            or self.assignment()
+            or self.expr()
+            or self.loops()
+            or self.conditional()
+        ):
             if self.if_then_next():
                 return True
         return False
-    
+
     # for multiple statements inside the if-block
     def if_then_next(self):
         if self.linebreak():
-            if self.tokens[self.i][1] == "NO WAI" or self.tokens[self.i][1] == "OIC" or self.if_then_codeblock():
+            if (
+                self.tokens[self.i][1] == "NO WAI"
+                or self.tokens[self.i][1] == "OIC"
+                or self.if_then_codeblock()
+            ):
                 return True
         return False
 
@@ -101,18 +121,17 @@ class Parser(object):
                                 else:
                                     return False
                             else:
-                                self.i -= 3 # point back since it did not match
+                                self.i -= 3  # point back since it did not match
                         elif self.tokens[self.i][1] == "OIC":
                             self.i += 1
                             return True
                     else:
-                        self.i -= 2 
+                        self.i -= 2
                 else:
                     self.i -= 1
             else:
                 self.i -= 1
         return False
-
 
     def switch_case(self):
         if self.tokens[self.i][1] == "WTF?":
@@ -149,16 +168,28 @@ class Parser(object):
                     self.i -= 1
         return False
 
-
     def switch_case_codeblock(self):
-        if self.tokens[self.i][1] == "GTFO" or self.output() or self.input() or self.declaration() or self.assignment() or self.expr() or self.loops() or self.conditional():
+        if (
+            self.tokens[self.i][1] == "GTFO"
+            or self.output()
+            or self.input()
+            or self.declaration()
+            or self.assignment()
+            or self.expr()
+            or self.loops()
+            or self.conditional()
+        ):
             if self.tokens[self.i][1] == "GTFO":
                 self.i += 1
             if self.linebreak():
-                if self.tokens[self.i][1] == "OIC" or self.tokens[self.i][1] == "OMGWTF" or self.switch_case_op() or self.switch_case_codeblock():
+                if (
+                    self.tokens[self.i][1] == "OIC"
+                    or self.tokens[self.i][1] == "OMGWTF"
+                    or self.switch_case_op()
+                    or self.switch_case_codeblock()
+                ):
                     return True
         return False
-
 
     def switch_case_op(self):
         if self.tokens[self.i][1] == "OMG":
@@ -166,9 +197,14 @@ class Parser(object):
             if self.literal():
                 if self.linebreak():
                     if self.switch_case_codeblock():
-                        if self.tokens[self.i][1] == "OIC" or self.tokens[self.i][1] == "OMGWTF" or self.switch_case_op():                            
+                        if (
+                            self.tokens[self.i][1] == "OIC"
+                            or self.tokens[self.i][1] == "OMGWTF"
+                            or self.switch_case_op()
+                        ):
                             return True
         return False
+
     #########################################################
 
     #########################################################
@@ -178,11 +214,17 @@ class Parser(object):
         if self.tokens[self.i][1] == "IM IN YR":
             self.i += 1
             if self.identifier():
-                if self.tokens[self.i][1] == "UPPIN" or self.tokens[self.i][1] == "NERFIN":
+                if (
+                    self.tokens[self.i][1] == "UPPIN"
+                    or self.tokens[self.i][1] == "NERFIN"
+                ):
                     self.i += 1
                     if self.yr_key():
                         if self.identifier():
-                            if self.tokens[self.i][1] == "TIL" or self.tokens[self.i][1] == "WILE":
+                            if (
+                                self.tokens[self.i][1] == "TIL"
+                                or self.tokens[self.i][1] == "WILE"
+                            ):
                                 self.i += 1
                                 if self.expr():
                                     if self.loops_next():
@@ -210,13 +252,19 @@ class Parser(object):
                 self.i -= 1
         return False
 
-
     def loops_codeblock(self):
-        if self.output() or self.input() or self.declaration() or self.assignment() or self.expr() or self.loops() or self.conditional():
+        if (
+            self.output()
+            or self.input()
+            or self.declaration()
+            or self.assignment()
+            or self.expr()
+            or self.loops()
+            or self.conditional()
+        ):
             if self.loops_next():
                 return True
         return False
-    
 
     def loops_next(self):
         if self.linebreak():
@@ -224,14 +272,14 @@ class Parser(object):
                 return True
         return False
 
-
     def yr_key(self):
         if self.tokens[self.i][1] == "YR":
             self.i += 1
             return True
-        return False      
+        return False
+
     #########################################################
-    
+
     #########################################################
     # ASSIGNMENT/DECLARATION/TYPECAST
 
@@ -247,7 +295,7 @@ class Parser(object):
             else:
                 self.i -= 1
                 return False
-    
+
     # variable declaration and initialization
     def declaration(self):
         if self.tokens[self.i][0] == "VARIABLE_DEC":
@@ -278,7 +326,7 @@ class Parser(object):
                 elif self.literal():
                     return True
                 else:
-                    return False 
+                    return False
         elif self.identifier():
             if self.tokens[self.i][1] == "IS NOW A":
                 self.i += 1
@@ -290,9 +338,10 @@ class Parser(object):
                     if self.literal():
                         return True
             else:
-                return False 
+                return False
         else:
             return False
+
     #########################################################
 
     #########################################################
@@ -307,7 +356,6 @@ class Parser(object):
                 return False
         else:
             return False
-    
 
     # print
     def output(self):
@@ -320,16 +368,17 @@ class Parser(object):
         else:
             return False
 
-    # for arguments of VISIBLE 
+    # for arguments of VISIBLE
     def print_arg(self):
         if self.linebreak():
             self.i -= 1
             return True
-        
+
         if self.identifier() or self.literal() or self.expr():
             self.print_arg()
             return True
         return False
+
     #########################################################
 
     #########################################################
@@ -339,12 +388,19 @@ class Parser(object):
         if self.tokens[self.i][0] == "BOOL_OP":
             if self.tokens[self.i][1] == "NOT":
                 self.i += 1
-                if self.comparison() or self.literal() or self.identifier() or self.relational_op():
+                if (
+                    self.comparison()
+                    or self.literal()
+                    or self.identifier()
+                    or self.relational_op()
+                ):
                     return True
                 else:
                     self.i -= 1
                     return False
-            elif self.tokens[self.i][1] == "ALL OF" or self.tokens[self.i][1] == "ANY OF":
+            elif (
+                self.tokens[self.i][1] == "ALL OF" or self.tokens[self.i][1] == "ANY OF"
+            ):
                 self.i += 1
                 if self.bool_operand() or self.all_any_arg():
                     if self.bool_arg():
@@ -369,13 +425,11 @@ class Parser(object):
                     self.i -= 1
         return False
 
-
     def bool_operand(self):
         if self.literal() or self.identifier() or self.comparison():
             return True
         return False
 
-    
     def bool_arg(self):
         if self.an_key():
             if self.bool_operand() or self.all_any_arg():
@@ -386,13 +440,18 @@ class Parser(object):
             return True
         return False
 
-    
     def all_any_arg(self):
-        if not (self.tokens[self.i][1] == "ALL OF" or self.tokens[self.i][1] == "ANY OF") and self.boolean():
+        if (
+            not (
+                self.tokens[self.i][1] == "ALL OF" or self.tokens[self.i][1] == "ANY OF"
+            )
+            and self.boolean()
+        ):
             return True
         return False
+
     #########################################################
-    
+
     #########################################################
     # CONCATENATION
 
@@ -403,7 +462,7 @@ class Parser(object):
                 if self.concat_arg():
                     return True
         return False
-            
+
     def concat_arg(self):
         if self.an_key():
             if self.identifier or self.literal():
@@ -413,8 +472,9 @@ class Parser(object):
                 elif self.tokens[self.i][1] == "EOL":
                     return True
         return False
+
     #########################################################
-        
+
     #########################################################
     # COMPARISON
 
@@ -433,7 +493,10 @@ class Parser(object):
 
     # for BIGGR OF and SMALLR OF arg in comparison
     def relational_op(self):
-        if self.tokens[self.i][1] == "BIGGR OF" or self.tokens[self.i][1] == "SMALLR OF":
+        if (
+            self.tokens[self.i][1] == "BIGGR OF"
+            or self.tokens[self.i][1] == "SMALLR OF"
+        ):
             self.i += 1
             if self.identifier() or self.math_literal() or self.arithmetic():
                 if self.an_key():
@@ -446,24 +509,35 @@ class Parser(object):
             else:
                 self.i -= 1
         return False
+
     #########################################################
 
     #########################################################
-    # ARITHMETIC 
+    # ARITHMETIC
 
     def arithmetic(self):
         if self.tokens[self.i][0] == "ARITHMETIC_OP":
             self.i += 1
-            if self.identifier() or self.literal() or self.math_typecast() or self.arithmetic():
+            if (
+                self.identifier()
+                or self.literal()
+                or self.math_typecast()
+                or self.arithmetic()
+            ):
                 if self.an_key():
-                    if self.identifier() or self.literal() or self.math_typecast() or self.arithmetic():
+                    if (
+                        self.identifier()
+                        or self.literal()
+                        or self.math_typecast()
+                        or self.arithmetic()
+                    ):
                         return True
                     else:
                         self.i -= 1
             else:
                 self.i -= 1
-        return False 
-    
+        return False
+
     # typecasting for arithmetic operations (to NUMBR/NUMBAR)
     def math_typecast(self):
         if self.tokens[self.i][1] == "MAEK":
@@ -476,7 +550,7 @@ class Parser(object):
                 elif self.math_type():
                     return True
                 else:
-                    return False 
+                    return False
         elif self.identifier():
             if self.tokens[self.i][1] == "IS NOW A":
                 self.i += 1
@@ -488,17 +562,15 @@ class Parser(object):
                     if self.math_type():
                         return True
             else:
-                return False 
+                return False
         else:
             return False
-       
 
     def math_type(self):
         if self.tokens[self.i][1] == "NUMBR" or self.tokens[self.i][1] == "NUMBAR":
             self.i += 1
             return True
-        return False             
-            
+        return False
 
     def math_literal(self):
         literal = ["NUMBR", "NUMBAR"]
@@ -506,9 +578,9 @@ class Parser(object):
             self.i += 1
             return True
         return False
+
     #########################################################
 
-    
     def checkErr(self):
         return self.errFlag
 
@@ -537,10 +609,7 @@ class Parser(object):
             self.i += 1
 
         err = "Error at" + lineError
-        self.console.delete("1.0", END)
-        self.console.insert(END, err)
-        self.console.config(state=DISABLED)
-
+        raise ParseError(err)
 
     def endprog(self):
         if self.tokens[self.i][1] == "KTHXBYE":
